a8ee5a25db36274e72239d129892945b8e139e54
Jonathan Tribouharet <jonathan@macbook.local>
2013-12-26 18:38:36 +0100

    Initial Commit

---
+++ Flooz/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Flooz
+//
+//  Created by jonathan on 12/26/2013.
+//  Copyright (c) 2013 Jonathan Tribouharet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
